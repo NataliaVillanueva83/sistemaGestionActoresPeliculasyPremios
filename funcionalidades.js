b0a94c79d9f894f93be8b7b49ec17a22e82563df
Natalia <nataliavillanueva83@hotmail.com>
2024-10-24 21:08:49 -0300

    Actualizo funciones del sistema

--- funcionalidades.js
+++ funcionalidades.js
@@ -7,15 +7,23 @@ function addAward (award, movie){
     movie.awards.push(award);
 }
 //ver peliculas mas premiadas
-function mostAwardedMovie(movies){
-
+function mostAwardedMovie(){
+//VER LAS PELICULAS MAS PREMIADAS
+let mostAwarded=[];
+let maxAwards=0;
+for(let movie of this.movies){
+    if(movie.awards.length>maxAwards){
+        maxAwards=movie.awards.length;
+        mostAwarded=[movie];
+    }else if(movie.awards.length===maxAwards){
+        mostAwarded.push(movie);
+    }
+}
+  console.tale (mostAwarded);
 }
 
 //busqueda de peliculas por genero
-function genreSearch (genero, peliculas){
-    return peliculas.filter(movie=> movie.genre===genero);
 
-}
 //busqueda de peliculas por actor
 function moviesByActor(actor, peliculas){
     return peliculas.filter(movie=> movie.actors.includes(actor));
@@ -40,14 +48,50 @@ function addMovie(){
 
 }
 //agregar actor
-function addActor(){
+function addActor(id, nombre){
     //agregar validacion para que no exista el actor
-    //agregar validacion para que no exista la pelicula
+    
     const actor= {
-        id: 'string',
-        name: 'string',
-        movies: []
+        id: id,
+        name: nombre,
+        
     }
+    //verificar si el actor esta --if verificarActor
+    this.actors.push(actor);
 }
 
 //funcion agregar actor a pelicula
+
+
+
+//busqueda de peliculas por genero
+function searchMoviesByGenre(genero, peliculas){
+    const resultado= peliculas.filter(movie=> movie.genre===genero);
+    console.table(resultado);
+    
+
+}
+//busqueda de peliculas por actor
+function moviesByActor(actor, peliculas){
+    return peliculas.filter(movie=> movie.actors.includes(actor));
+}
+//cuantos premios gano una pelicula
+function awardsPerMovie(movie){
+    return movie.awards.length;
+}
+function  seeAdwardsbyMovie (title){
+
+}
+//agregar pelicula
+function addMovie(title, genre, actors){
+ //agregar validacion para que no exista la pelicula
+ const movie= {
+     title: title,
+     genre: genre,
+     actors: [],
+     awards: [],
+ };
+this.movies.push(movie);    
+     
+}
+
