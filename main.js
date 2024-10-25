7b5da243c52ead467a8174b197660bef85a6659e
Natalia <nataliavillanueva83@hotmail.com>
2024-10-24 21:09:54 -0300

    Agrego bosquejo del menu principal

---
+++ main.js
@@ -0,0 +1,28 @@
+//armar menu
+// A. menu principal, B. menu administrador
+
+/**
+ * Menu principal
+ * 1. Buscar por nombre de pelicula
+ * 2. Busqueda por genero //submenu ---desea ver el menu o ingrese el genero que quiera ver
+ *   2.a. Ciencia ficcion
+ *   2.b. Drama
+ *   2.c. Comedia
+ *   2.d. Accion
+ *   2.e. Terror
+ *   2.f. Crimen
+ *   2.g. thriller
+ *   2.h. Infantil
+ * 3. Busqueda de peliculas por actor
+ * 4. Ver peliculas mas premiadas
+ * 5. Ver premios por pelicula
+ * 
+ *  */
+/**
+ * Menu administrador
+ * a. Agregar pelicula
+ * b. Editar pelicula
+ * c. Agregar premio a pelicula
+ * d. Eliminar actor de pelicula
+ * e. Eliminar pelicula de la base
+ */
